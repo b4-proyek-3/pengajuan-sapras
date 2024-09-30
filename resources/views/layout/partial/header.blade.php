2de3bff5d88a51a8830705dcaf05338a504a0570
Yani Rahmawati <yaniafan04@gmail.com>
2024-09-30 14:01:12 +0700

    Detail Pengajuan

--- resources/views/layout/partial/header.blade.php
+++ resources/views/layout/partial/header.blade.php
@@ -2,9 +2,12 @@
 <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
       <div class="h-19.5">
         <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
-        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
-          <img src="./assets/img/logo-ct.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
-          <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Soft UI Dashboard</span>
+        <a class="flex items-center px-8 py-4 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
+            <img src="./assets/img/polban.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-10 w-10" alt="main_logo" />
+            <div class="ml-2">
+                <span class="block font-semibold transition-all duration-200 ease-nav-brand">Pengajuan</span>
+                <span class="block font-semibold transition-all duration-200 ease-nav-brand">Sarana dan Prasarana</span>
+            </div>
         </a>
       </div>
 
@@ -56,7 +59,7 @@
                   </g>
                 </svg>
               </div>
-              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Tables</span>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Status Pengajuan</span>
             </a>
           </li>
 
@@ -77,7 +80,7 @@
                   </g>
                 </svg>
               </div>
-              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Billing</span>
+              <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Riwayat Pengajuan</span>
             </a>
           </li>
 
