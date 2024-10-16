37705e8c396db1d23810a9e277c9b5a14180d4e5
Yani Rahmawati <yaniafan04@gmail.com>
2024-10-16 07:19:10 +0700

    Perbaikan Sidebar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -17,7 +17,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/polban.png" />
+    <link rel="logo-polban" sizes="16x16" href="./assets/img/polban.png" />
     <link rel="icon" type="image/png" href="./assets/img/polban.png" />
     <title>Pengajuan Sarana dan Prasarana</title>
     @include('layout.partial.link')
--- resources/views/layout/partial/header.blade.php
+++ resources/views/layout/partial/header.blade.php
@@ -1,5 +1,5 @@
 <!-- sidenav  -->
-<aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-full h-screen -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
+  <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-full h-128 -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
       <div class="h-19.5">
         <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
         <a class="flex items-center px-8 py-4 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
@@ -13,7 +13,7 @@
 
       <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
 
-      <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+      <div class="items-center block w-auto h-sidenav grow basis-full">
         <ul class="flex flex-col pl-0 mb-0">
           <li class="mt-0.5 w-full">
             <a class="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors" href="./pages/dashboard.html">
