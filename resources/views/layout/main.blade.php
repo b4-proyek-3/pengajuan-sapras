6b1a6b17ca980d96eb4748c8d89ac79665eb832b
Yani Rahmawati <yaniafan04@gmail.com>
2024-10-16 07:26:17 +0700

    Merge main.php dari detail-pengajuan

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -17,7 +17,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/polban.png" />
+    <link rel="logo-polban" sizes="16x16" href="./assets/img/polban.png" />
     <link rel="icon" type="image/png" href="./assets/img/polban.png" />
     <title>Pengajuan Sarana dan Prasarana</title>
     @include('layout.partial.link')
@@ -25,7 +25,7 @@
 
   <body class="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500">
     @include('layout.partial.header')
-      @yield('content')s
+      @yield('content')
     @include('layout.partial.footer')
   </body>
   @include('layout.partial.script')
