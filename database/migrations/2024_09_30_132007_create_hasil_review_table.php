7d457194c54c0b4c84ac35f21aff26405e6eda28
n4zla <nazla.kayla05@gmail.com>
2024-11-13 15:15:35 +0700

    Mengambil file migrations dari branch feature/detail-pengajuan

---
+++ database/migrations/2024_09_30_132007_create_hasil_review_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->string('id_pengajuan', length: 6); // Foreign key ke tabel pengajuan
+            $table->foreign('id_pengajuan')->references('id_pengajuan')->on('pengajuan')->onDelete('cascade');
+            $table->char('id_reviewer', length: 18); // Foreign key ke tabel reviewer
+            $table->foreign('id_reviewer')->references('id_reviewer')->on('reviewers')->onDelete('cascade');
+            $table->text('review')->nullable();
+            $table->enum('status', ['diterima', 'direvisi', 'ditolak', 'selesai']);
+            $table->dateTime('tanggal_review', precision: 0);
+
+            $table->primary(['id_pengajuan', 'id_reviewer']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hasil_review');
+    }
+};
