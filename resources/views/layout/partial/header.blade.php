9d5fba06e86ad1d1e473f90f3d4d0a96bbd1d9b2
Yani Rahmawati <yaniafan04@gmail.com>
2024-11-19 07:42:38 +0700

    css: perbaikan file header

--- resources/views/layout/partial/header.blade.php
+++ resources/views/layout/partial/header.blade.php
@@ -1,7 +1,7 @@
 <!-- sidenav  -->
 <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-full h-128 -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
       <div class="h-19.5">
-        <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
+        <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas text-slate-400 xl:hidden" sidenav-close></i>
         <a class="flex items-center px-8 py-4 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
             <img src={{ asset('assets/img/polban.png') }} class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-10 w-10" alt="main_logo" />
             <div class="ml-2">
