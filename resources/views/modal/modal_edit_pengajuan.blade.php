9d05a8059a64bfc42f3d7cd545da03b70e2de526
dheaasprla <dsd06177@gmail.com>
2024-11-20 09:02:56 +0700

    perubahan checkout sebelum ke bracnh detail pengajuan

--- resources/views/modal/modal_edit_pengajuan.blade.php
+++ resources/views/modal/modal_edit_pengajuan.blade.php
@@ -1,29 +1,21 @@
 <div id="editPengajuanModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-100 flex justify-center items-center w-full h-full">
     <div class="fixed inset-0 bg-gray-800 opacity-50"></div>
     <div class="fixed p-2 w-full w-2/5 mx-auto">
+        <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
             <div class="flex items-center justify-between p-4 mb-0 border-b rounded-t dark:border-gray-600">
                 <h3 class="text-xl mb-0 font-semibold text-gray-900 dark:text-white">
                     Edit Pengajuan
                 </h3>
             </div>
+            <!-- Modal body -->
             <div class="p-4 md:p-5">
-<<<<<<< HEAD
-                <form class="space-y-4" action="{{ route('pengajuan.update', $pengajuans->id_pengajuan) }}" method="POST">
-=======
                 <form class="space-y-4" action="{{ route('pengaju.update', $pengajuans->id_pengajuan) }}" method="POST">
->>>>>>> feature/detail-pengajuan
                     @csrf
                     @method('PUT')
                     <div class="flex flex-col">
                         <label for="nama-pengaju" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Pengaju</label>
-<<<<<<< HEAD
-                        <input type="text" id="nama-pengaju" name="nama_pengaju" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->pengaju->nama }}" />
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="ormawa" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Ormawa</label>
-                        <input type="text" id="ormawa" name="nama_ormawa" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->pengaju->ormawa->nama_ormawa ?? 'N/A' }}" />
-=======
                         <input id="nama-pengaju" value="{{ $pengajuans->pengaju->nama }}" class="bg-gray-50 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" readonly/>
                     </div>
                     <div class="flex flex-col">
@@ -55,39 +47,13 @@
                                 </option>
                             @endforeach
                         </select>
->>>>>>> feature/detail-pengajuan
                     </div>
-                    <div class="flex flex-col">
-                        <label for="nama-kegiatan" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Kegiatan</label>
-                        <input type="text" id="nama-kegiatan" name="nama_kegiatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->nama_kegiatan }}" />
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="tanggal-kegiatan" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Kegiatan</label>
-                        <input type="date" id="tanggal-kegiatan" name="tanggal_kegiatan" class="bg-gray-50 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->tanggal_pinjam }}" />
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="tanggal-berakhir" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Berakhir</label>
-                        <input type="date" id="tanggal-berakhir" name="tanggal_berakhir" class="bg-gray-50 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->tanggal_akhir }}" />
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="waktu-kegiatan" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu Kegiatan</label>
-                        <input type="time" id="waktu-kegiatan" name="waktu_kegiatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="{{ $pengajuans->waktu_pengajuan }}" />
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="tempat-kegiatan" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Tempat Kegiatan</label>
-                        <select name="id_tempat" class="bg-gray-50 px-3 border border-gray-300 text-gray-900 text-sm rounded-lg">
-                            @foreach ($tempatList as $tempat)
-                                <option value="{{ $tempat->id_tempat }}" {{ $tempat->id_tempat == $pengajuans->id_tempat ? 'selected' : '' }}>
-                                    {{ $tempat->nama_tempat }}
-                                </option>
-                            @endforeach
-                        </select>                    </div>
                     <div class="flex justify-end">
                         <button type="submit" class="mt-2 bg-gradient-to-tl from-blue-600 to-teal-400 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white">
-                            SIMPAN
+                            simpan
                         </button>
                         <button type="button" class="mt-2 bg-gradient-to-tl from-slate-600 to-slate-300 px-2.5 text-xs rounded-1.8 py-1.4 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white ml-2" onclick="closeModal('editPengajuanModal')">
-                            BATAL
+                            batal
                         </button>
                     </div>
                 </form>
--- resources/views/pengaju/detail.blade.php
+++ resources/views/pengaju/detail.blade.php
@@ -164,23 +164,23 @@
               <div class="flex-auto p-4">
                 <div class="before:border-r-solid relative before:absolute before:top-0 before:left-4 before:h-full before:border-r-2 before:border-r-slate-100 before:content-[''] before:lg:-ml-px">
                   <div class="relative mb-4 mt-0 after:clear-both after:table after:content-['']">
-                  @foreach ($pengajuans->latestReview as $index => $review)
-                      <span class="w-6.5 h-6.5 text-base absolute left-4 z-10 inline-flex -translate-x-1/2 items-center justify-center rounded-full bg-white text-center font-semibold">
-                          <i class="relative z-10 leading-none text-transparent 
-                            {{ $index === 0 ? 'ni ni-bell-55 bg-gradient-to-tl from-green-600 to-lime-400' : 'ni ni-time-alarm bg-gradient-to-tl from-blue-600 to-indigo-400' }} 
-                            leading-pro bg-clip-text fill-transparent"></i>
-                      </span>
-                      <div class="ml-11.252 pt-1.4 lg:max-w-120 relative -top-1.5 w-auto">
-                          <h6 class="mb-0 text-sm font-semibold leading-normal text-slate-700">{{ $review->reviewer->roles->nama_role ?? 'N/A' }}</h6>
-                          <p class="mt-1 mb-0 text-xs font-semibold leading-tight text-slate-400">{{ $review->status ?? 'N/A' }}</p>
-                          <p class="mt-1 mb-0 text-xs font-semibold leading-tight text-slate-400">{{ $review->tanggal_review ?? 'N/A' }}</p>
-                      </div>
-                  @endforeach
+                  @if($pengajuans->latestReview->isNotEmpty())
+                      @foreach ($pengajuans->latestReview as $index => $review)
+                          <span class="w-6.5 h-6.5 text-base absolute left-4 z-10 inline-flex -translate-x-1/2 items-center justify-center rounded-full bg-white text-center font-semibold">
+                              <i class="relative z-10 leading-none text-transparent ni ni-time-alarm bg-gradient-to-tl from-blue-600 to-indigo-400 
+                                leading-pro bg-clip-text fill-transparent"></i>
+                          </span>
+                          <div class="ml-11.252 pt-1.4 lg:max-w-120 relative -top-1.5 w-auto">
+                              <h6 class="mb-0 text-sm font-semibold leading-normal text-slate-700">{{ $review->reviewer->roles->nama_role ?? 'N/A' }}</h6>
+                              <p class="mt-1 mb-0 text-xs font-semibold leading-tight text-slate-400">{{ $review->status ?? 'N/A' }}</p>
+                              <p class="mt-1 mb-0 text-xs font-semibold leading-tight text-slate-400">{{ $review->tanggal_review ?? 'N/A' }}</p>
+                          </div>
+                      @endforeach
+                    @endif
                   </div>
                   <div class="relative mb-4 mt-0 after:clear-both after:table after:content-['']">
                       <span class="w-6.5 h-6.5 text-base absolute left-4 z-10 inline-flex -translate-x-1/2 items-center justify-center rounded-full bg-white text-center font-semibold">
-                          <i class="relative z-10 leading-none text-transparent 
-                            {{ $index === 0 ? 'ni ni-bell-55 bg-gradient-to-tl from-green-600 to-lime-400' : 'ni ni-time-alarm bg-gradient-to-tl from-blue-600 to-indigo-400' }} 
+                          <i class="relative z-10 leading-none text-transparent ni ni-bell-55 bg-gradient-to-tl from-green-600 to-lime-400
                             leading-pro bg-clip-text fill-transparent"></i>
                       </span>
                       <div class="ml-11.252 pt-1.4 lg:max-w-120 relative -top-1.5 w-auto">
